b23755be45d5b295bd4f6ab06a796be5c5ef0c2e
Deepak Lohar <deepaklohar1993@gmail.com>
2022-03-17 19:48:12 +0530

    Added new class IntegerStack and StringStack

---
+++ GenericExample/GenericExample/IntegerStack.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericExample
+{
+    public class IntegerStack 
+    {
+        private int[] stack;
+        public int Position { get; private set; }   
+        public void Push(int value) { throw new NotImplementedException(); }
+        public int Pop() { throw new NotImplementedException();  }
+
+        
+    }
+
+   
+}
---
+++ GenericExample/GenericExample/StringStack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericExample
+{
+    public class StringStack
+    {
+        private int[] stack;
+        public int Position { get; private set; }
+        public void Push(string value) { throw new NotImplementedException(); }
+        public string Pop() { throw new NotImplementedException(); }
+    }
+}
