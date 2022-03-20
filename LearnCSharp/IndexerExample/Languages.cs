fb990d913373f6b401644d5b6843f04d62f38b7f
Deepak Lohar <deepaklohar1993@gmail.com>
2022-03-20 17:21:57 +0530

    Indexer Example

---
+++ LearnCSharp/IndexerExample/Languages.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexerExample
+{
+    internal class Languages
+    {
+        string[] languages = new string[100];
+
+        public string this[int index] {
+
+            get { return languages[index]; }
+       
+            set { languages[index] = value; }
+        }
+    }
+}
--- LearnCSharp/IndexerExample/Program.cs
+++ LearnCSharp/IndexerExample/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+//Create the object of class
+
+using IndexerExample;
+
+Languages languages = new Languages();
+
+languages[0] = "Marathi";
+languages[1] = "Hindi";
+languages[2] = "English";
+languages[3] = "French";
+
+Console.WriteLine(languages[2]); 
+
+Console.ReadKey();
